b3bb18d93e6d31d507223c7177500cbdd9f21b1f
dsd739 <durgadangi.2021cs@technonjr.org>
2023-01-11 20:41:29 +0530

    First commit
    it's a news website using React class based Component
    
     Changes to be committed:
    	modified:   public/index.html
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/Components/Navbar.js
    	new file:   src/Components/News.css
    	new file:   src/Components/News.js
    	new file:   src/Components/Newsitem.js
    	modified:   src/index.css
    	deleted:    src/logo.svg

---
+++ src/Components/Newsitem.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+
+export class Newsitem extends Component {
+  
+  render() { 
+    let {title,description,imageUrl}=this.props;
+    return (
+      <div className="card" style={{width: '18rem'}}>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">
+            {description}
+          </p>
+          <a href="/" className="btn btn-sm btn-primary">
+            <strong>Read</strong> More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Newsitem;
