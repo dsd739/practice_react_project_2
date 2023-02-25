6da7a9802dacb9c6cfb98c6e74479e7354a34046
dsd739 <durgadangi.2021cs@technonjr.org>
2023-02-25 15:22:13 +0530

    addeded Categories of news and remove all console.log to clear code  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/Components/Navbar.js 	modified:   src/Components/News.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,106 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { ink } from "react-router-dom";
 export class Navbar extends Component {
-  
-
   render() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary stickey">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">NewsMonkey</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="/">
+            NewsMonkey
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
               </li>
-              
-             
               <li className="nav-item">
-                <a className="nav-link disabled"href='/'>About</a>
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/business"
+                >
+                  business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/entertainment"
+                >
+                  entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/general"
+                >
+                  general
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/health"
+                >
+                  health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/sports"
+                >
+                  sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/technology"
+                >
+                  technology
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/science"
+                >
+                  science
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link disabled" href="/">
+                  About
+                </a>
               </li>
             </ul>
-            
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Loader from "./Loader";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    category: "general",
+  };
+  static propTypes = {
+    category: PropTypes.string,
+  };
+
   constructor() {
     super(); //  Must call super constructor in derived class before accessing 'this' or returning from derived constructor
-    console.log("in the constructerr");
     this.state = {
       articles: null, // this.articles yani is class ke upper jo array hai vo abb iske state ke pass ja chuka hai
       loading: false,
@@ -15,66 +22,50 @@ export class News extends Component {
   }
   async componentDidMount() {
     this.setState({ loading: true });
-    console.log("in the cdm first");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
-    console.log(url);
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
     let data = await fetch(url);
-    let parsedata = await data.json();
-    console.log(parsedata);
+    let parsedata = await data.json();console.log(parsedata);
     this.setState({
       articles: parsedata.articles,
       totalNews: parsedata.totalResults,
       loading: false,
     });
-    console.log("in the cdm last");
   }
 
   handlepreviousclick = async () => {
     this.setState({ loading: true });
-    console.log("in the privious first");
-    console.log("privious clicked");
     await this.setState({
       page: this.state.page - 1,
     });
-    console.log(this.state.page);
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
-    console.log(url);
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
     let data = await fetch(url);
     let parsedata = await data.json();
-    console.log(parsedata);
     this.setState({
       articles: parsedata.articles,
       loading: false,
     });
-    console.log("in the privious last");
   };
   handlenextclick = async () => {
     this.setState({ loading: true });
-    console.log("next click");
-    console.log("in the next first");
     await this.setState({
       page: this.state.page + 1,
     });
-    console.log(this.state.page);
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
-    console.log(url);
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=46eca8ae2a5247228a16ca209143fb35&pagesize=15&page=${this.state.page}`;
     let data = await fetch(url);
     let parsedata = await data.json();
-    console.log(parsedata);
     this.setState({
       articles: parsedata.articles,
       loading: false,
     });
-    console.log("in the next last");
   };
 
   render() {
     return (
       <>
         {this.state.loading && <Loader />}
-        <h2 className="container">NewsMonkey-Top Headlines</h2>
+        <h2 className="container heading">NewsMonkey-Top Headlines</h2>
         <div className="container my-3">
-          {this.state.articles
+          {!this.state.loading && this.state.articles
             ? this.state.articles.map((e) => {
                 // console.log(e);
                 // mtlb jo is class ke state mai articles(group of objects) pda hua hai use har object ke liye is jsx ko put kro
